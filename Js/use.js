17a483774ebb30a1524d7d671c3f664082890283
EmreTepetam <emretepetam@gmail.com>
2024-09-01 21:24:54 +0300

    mesaj kutusu

--- Js/use.js
+++ Js/use.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
     $('#welcomeModal').modal('show');
-  });
+
+    var collapseElement = document.getElementById('toggle-icon');
+
+    if (collapseElement) {
+        collapseElement.addEventListener('show.bs.collapse', function () {
+            document.getElementById('toggle-icon').classList.remove('fa-chevron-up');
+            document.getElementById('toggle-icon').classList.add('fa-chevron-down');
+        });
+
+        collapseElement.addEventListener('hide.bs.collapse', function () {
+            document.getElementById('toggle-icon').classList.remove('fa-chevron-down');
+            document.getElementById('toggle-icon').classList.add('fa-chevron-up');
+        });
+    }
+});
